2a3503d78f31c0fb914c657ef5889fdbe787b0fd
Milan Wijnmaalen <info@milanwijnmaalen.com>
2018-11-13 15:57:51 +0100

    Moved Ray and Color to its own .h

--- Ray.h
+++ Ray.h
@@ -1,3 +1,25 @@
 #pragma once
-#include "precomp.h"
 
+struct Ray
+{
+	vec3 Origin = { 0,0,0 };
+	vec3 Direction;
+};
+
+//To support intermediate results > 255, store colors as 3 uints. This is probably terrible performance wise.
+struct Color
+{
+	uint R = 0;
+	uint G = 0;
+	uint B = 0;
+
+	uint to_uint() {
+		return ((R & 255) << 16) + ((G & 255) << 8) + (B & 255);
+	}
+
+	void from_uint(uint color) {
+		B = color & 255;
+		G = (color >> 8) & 255;
+		R = (color >> 16) & 255;
+	}
+};
